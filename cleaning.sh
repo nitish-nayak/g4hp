c93cfc2b3033e79323c47140df3ed58f6c340242
Leonidas Aliaga Soplin <laliaga@dunegpvm06.fnal.gov>
2020-06-24 21:10:46 -0500

    more deleting

--- cleaning.sh
+++ cleaning.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 make clean
-rm -r build CMakeFiles CMakeCache.txt cmake_install.cmake g4hpCint_rdict.pcm
+rm -r build CMakeFiles CMakeCache.txt cmake_install.cmake g4hpCint_rdict.pcm Makefile
